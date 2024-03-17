5625d6c5a40958a57d16279342380d9df9d166e3
Uriel Procópio <procopiouriel928@gmail.com>
2024-03-17 11:48:55 -0400

    first commit

---
+++ Moq-Framework/Interface/IGerarHorario.cs
@@ -0,0 +1,5 @@
+﻿
+public interface IGerarHorario
+{
+    bool ReturnVolta();
+}
---
+++ Moq-Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+
+namespace Moq_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mockMinhaInterface = new Mock<IGerarHorario>();
+            mockMinhaInterface.Setup(m => m.ReturnVolta()).Returns(false);
+            var minhaClasse = new GerarHorario(mockMinhaInterface.Object);
+
+            // Act
+            bool resultado = minhaClasse.ReturnVolta();
+
+            // Assert
+            Assert.IsFalse(resultado);
+
+
+        }
+    }
+}
